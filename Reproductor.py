4be2d0e1da82e29c9789b8adbdd832c6e0d9ff96
Pedro Perdomo <pedroperdomo@ejemplo.com>
2024-11-11 23:32:09 -0500

    agregue el frame y los iconos

--- Reproductor.py
+++ Reproductor.py
@@ -9,4 +9,21 @@ class Reproductor():
         self.ventana.config(width=800, height=600)
         self.ventana.resizable(0,0)
 
+        #frame
+        self.frameBorde = tk.Frame(self.ventana, bd=5, relief="ridge", bg="black")
+        self.frameBorde.place(relx=0.20, rely=0.20, width=500, height=200)
+        self.frame = tk.Frame(self.frameBorde)
+        self.frame.place(width=490, height=190)
+
+        #Iconos 
+        self.play = tk.PhotoImage(file=r"Reproductor/iconos/play.png")
+        self.pause = tk.PhotoImage(file=r"Reproductor/iconos/pause.png")
+        self.skip2 = tk.PhotoImage(file=r"Reproductor/iconos/play-skip2.png")
+        self.skip = tk.PhotoImage(file=r"Reproductor/iconos/play-skip.png")
+        self.back = tk.PhotoImage(file=r"Reproductor/iconos/back.png")
+
+        #Botones
+        self.btnPlay = tk.Button(self.ventana, image=self.play)
+        self.btnPlay.place(relx=0.5, rely=0.65, width=40, height=40)
+
         self.ventana.mainloop()
