2fcb4db8327aebc83e4fe9e56574a9fb700db5f8
Pedro Perdomo <pedroperdomo@ejemplo.com>
2024-11-12 11:21:49 -0500

    Agregue un icono nuevo, Agregue un boton menu y agregue un color a los botones y la ventana

--- Reproductor.py
+++ Reproductor.py
@@ -11,6 +11,7 @@ class Reproductor():
         self.ventana = tk.Tk()
         self.ventana.title("Reproductor de Musica")
         self.ventana.config(width=700, height=600)
+        self.ventana.config(bg="#FFFFFF")
         self.ventana.resizable(0,0)
 
         # Variables de control
@@ -18,7 +19,7 @@ class Reproductor():
         #frame
         self.frameBorde = tk.Frame(self.ventana, bd=5, relief="ridge", bg="black")
         self.frameBorde.place(relx=0.15, rely=0.15, width=500, height=240)
-        self.frame = tk.Frame(self.frameBorde)
+        self.frame = tk.Frame(self.frameBorde, bg="#FFFFFF")
         self.frame.place(width=490, height=230)
 
         #Iconos 
@@ -28,27 +29,31 @@ class Reproductor():
         self.skip = tk.PhotoImage(file=r"Reproductor/iconos/play-skip.png")
         self.back = tk.PhotoImage(file=r"Reproductor/iconos/back.png")
         self.back2 = tk.PhotoImage(file=r"Reproductor/iconos/back2.png")
+        self.menu = tk.PhotoImage(file=r"Reproductor/iconos/menu.png")
 
         #Botones
-        self.btnPlay = tk.Button(self.ventana, image=self.play)
+        self.btnPlay = tk.Button(self.ventana, image=self.play, bg="#FFFFFF")
         self.btnPlay.place(relx=0.5, rely=0.76, width=40, height=40, anchor="center")
 
-        self.btnSkip = tk.Button(self.ventana, image=self.skip)
+        self.btnSkip = tk.Button(self.ventana, image=self.skip, bg="#FFFFFF")
         self.btnSkip.place(relx=0.60, rely=0.76, width=40, height=40, anchor="center")
 
-        self.btnSkip2 = tk.Button(self.ventana, image=self.skip2)
+        self.btnSkip2 = tk.Button(self.ventana, image=self.skip2, bg="#FFFFFF")
         self.btnSkip2.place(relx=0.40, rely=0.76, width=40, height=40, anchor="center")
 
-        self.btnBack2 = tk.Button(self.ventana, image=self.back2)
+        self.btnBack2 = tk.Button(self.ventana, image=self.back2, bg="#FFFFFF")
         self.btnBack2.place(relx=0.33, rely=0.76, width=40, height=40, anchor="center")
 
-        self.btnBack = tk.Button(self.ventana, image=self.back)
+        self.btnBack = tk.Button(self.ventana, image=self.back, bg="#FFFFFF")
         self.btnBack.place(relx=0.67, rely=0.76, width=40, height=40, anchor="center")
+
+        self.btnMenu = tk.Button(self.ventana, image=self.menu, bg="#FFFFFF")
+        self.btnMenu.place(relx=0.80, rely=0.56, width=42, height=25)
 
         # Barra de progreso 
         self.barra = tk.Canvas(self.ventana, width=509, height=10, bg="#555", bd=0, relief="flat")
         self.barra.place(relx=0.14, rely=0.67)
-        self.barrarogreso = self.barra.create_rectangle(0, 0, 0, 10, fill="#1db954", outline="") 
+        self.barraprogreso = self.barra.create_rectangle(0, 0, 0, 10, fill="#1db954", outline="") 
 
 
         self.ventana.mainloop()
